3eaa4f2d5e436f68dbdf4f2ce52fe2c4be67bf11
Misha Solovtsov <mikhail.solovtsov@gmail.com>
2024-09-17 10:14:48 +0300

    Change googletest vertion,  added exceptions in main(),  fixed file system paths

--- src/main.cpp
+++ src/main.cpp
@@ -10,32 +10,43 @@ int main() {
 
     auto t_start = std::chrono::high_resolution_clock::now();
 
-    // Заполняет vector текстом из файла
-    InvertedIndex idx;
-    idx.UpdateDocumentBase(ConverterJSON::GetTextDocuments());
-    // Поиск
-    SearchServer srv(idx);
-    std::vector<std::vector<RelativeIndex>> result = srv.search(
-            ConverterJSON::GetRequests(),
-            ConverterJSON::GetResponsesLimit()
-            );
+    try {
+        // Заполняет vector текстом из файла
+        InvertedIndex idx;
+        idx.UpdateDocumentBase(ConverterJSON::GetTextDocuments());
+        // Поиск
+        SearchServer srv(idx);
+        std::vector<std::vector<RelativeIndex>> result = srv.search(
+                ConverterJSON::GetRequests(),
+                ConverterJSON::GetResponsesLimit()
+        );
 
-    // Преобразуем результат в понятный answers.json формат
-    std::vector<std::vector<std::pair<int, float>>> buf;
-    for (auto & i : result){
-        std::vector<std::pair<int, float>> buf1;
-        for (auto & j : i){
-            int id = j.doc_id;
-            float rank = j.rank;
-            buf1.emplace_back(id, rank);
+        // Преобразуем результат в понятный answers.json формат
+        std::vector<std::vector<std::pair<int, float>>> buf;
+        for (auto &i: result) {
+            std::vector<std::pair<int, float>> buf1;
+            for (auto &j: i) {
+                int id = j.doc_id;
+                float rank = j.rank;
+                buf1.emplace_back(id, rank);
+            }
+            buf.push_back(buf1);
+            buf1.clear();
         }
-        buf.push_back(buf1);
-        buf1.clear();
+        // Заполняем answers.json
+        ConverterJSON::putAnswers(buf);
+        buf.clear();
+        result.clear();
+
+    } catch (const nlohmann::json::exception &e) {
+        std::cerr << "the json exception\n"
+                     "something is wrong with the json files:\n" << e.what() << std::endl;
+    }catch (const std::exception& x){
+        std::cerr << "the standard exception\n"
+                     "something is wrong: \n" << x.what() << std::endl;
+    }catch (...){
+        std::cerr << "something is wrong..." << std::endl;
     }
-    // Заполняем answers.json
-    ConverterJSON::putAnswers(buf);
-    buf.clear();
-    result.clear();
 
     auto t_end = std::chrono::high_resolution_clock::now();
 
